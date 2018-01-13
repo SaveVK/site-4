d2cfdde9ca4254df61510b467bf982ef704e801d
QWERT <microsoftman535@gmail.com>
2018-01-13 18:26:43 +0200

    init 2

--- mail.php
+++ mail.php
@@ -14,13 +14,13 @@ $location = $_POST['user_location_address'];
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = '5starshopuae@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'terry123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = '@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'password'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
-$mail->setFrom('5starshopuae@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('5starshopuae@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com')
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -29,7 +29,7 @@ $mail->addAddress('5starshopuae@gmail.com');     // Кому будет уход
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
 $mail->isHTML(true);
 
-$mail->Subject = 'new message from wirelesscharger.com';
+$mail->Subject = 'new message from ';
 $mail->Body    = 'Name: ' .$name . '<br>Number:' .$number. '<br>Email:' .$email. '<br>Location:' .$location;
 $mail->AltBody = '';
 
